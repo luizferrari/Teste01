8fddc48a642dee4c4153f330bcf7269b419de1de
luiz_ferrari <luiz_ferrari@10.1.48.157>
2014-05-27 15:36:40 -0300

    Inicialização do projeto

---
+++ src/teste01/Teste01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author luiz_ferrari
+ */
+public class Teste01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+        JOptionPane.showMessageDialog(null,"he3he3brbr");
+    }
+}
